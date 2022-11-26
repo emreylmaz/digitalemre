c993d62663f82b5c6bc1d765ef865fed2984464e
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-11-26 23:47:40 +0300

    eslint disable for configration

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
+const eslintConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
 
 const withTM = require('next-transpile-modules')(["react-icons"]);
 
-module.exports =  withTM({nextConfig});
+module.exports =  withTM({nextConfig,eslintConfig});
