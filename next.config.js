979386f64e8d89b3a58a5d7ff3d5d3ff64fd3da7
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-11-26 23:35:50 +0300

    initialize

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+const withTM = require('next-transpile-modules')(["react-icons"]);
+
+module.exports =  withTM({nextConfig});
